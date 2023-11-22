2f6a9a961d12fa801feaf96aa1cb082c85dff7a7
Stosh09 <stoshochieng@gmail.com>
2023-11-23 02:47:10 +0300

    Finishes updating classes by updating db on state and user classes

--- models/state.py
+++ models/state.py
@@ -1,8 +1,32 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
 from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from models.city import City
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship, backref
+from os import getenv
 
 
-class State(BaseModel):
+class State(BaseModel, Base):
     """ State class """
-    name = ""
+    __tablename__ = "states"
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref=backref(
+            "state", cascade="all, delete"))
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """getter attribute that returns a list of city
+            instances with state_id equals to the current state.id
+            """
+            from models import storage
+            related_cities = []
+            my_cities = storage.all(City)
+            for city in my_cities.values():
+                if self.id == city.state_id:
+                    related_cities.append(city)
+            return related_cities
