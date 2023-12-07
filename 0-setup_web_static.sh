5b05f9438bf190eb149a8033edc455a22e0e6805
Stosh09 <stoshochieng@gmail.com>
2023-12-08 02:25:28 +0300

    Sets up web-server for deployment of web-static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,38 +1,36 @@
 #!/usr/bin/env bash
-# Sets up a web server for deployment of web_static.
+# Bash script that sets up your web servers for the deployment of web_static
 
-apt-get update
-apt-get install -y nginx
+WEB_STATIC="location /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n"
 
-mkdir -p /data/web_static/releases/test/
-mkdir -p /data/web_static/shared/
-echo "Holberton School" > /data/web_static/releases/test/index.html
-ln -sf /data/web_static/releases/test/ /data/web_static/current
+#install nginx if not yet installed
+sudo apt-get update > /dev/null 2>&1
+sudo apt-get -y install nginx > /dev/null
 
-chown -R ubuntu /data/
-chgrp -R ubuntu /data/
+# Create the folders /data/web_static/ if it doesn’t already exist
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
 
-printf %s "server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    add_header X-Served-By $HOSTNAME;
-    root   /var/www/html;
-    index  index.html index.htm;
+# Create a fake HTML file /data/web_static/releases/test/index.html
+if [ -L "/data/web_static/current" ];
+then
+    sudo rm -f /data/web_static/current
+fi
+echo "<html>
+    <head>
+    </head>
+    <body>
+        Holberton School
+    </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
 
-    location /hbnb_static {
-        alias /data/web_static/current;
-        index index.html index.htm;
-    }
+#Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder.
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
 
-    location /redirect_me {
-        return 301 http://cuberule.com/;
-    }
+# Grant ownership of the /data/ folder to the ubuntu user AND group. This should be recursive
+sudo chown -R ubuntu:ubuntu /data/
 
-    error_page 404 /404.html;
-    location /404 {
-      root /var/www/html;
-      internal;
-    }
-}" > /etc/nginx/sites-available/default
-
-service nginx restart
+# Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static
+sudo sed -i "0,/location/s|location|$WEB_STATIC\n\t&|" /etc/nginx/sites-available/default
+# Restart Nginx to apply the changes
+sudo service nginx restart > /dev/null
